bb1778531483343a098232388b8cb712554ba3a3
Hugo <hugochen1212@gmail.com>
2017-12-08 13:07:58 -0800

    add basicScreens with contacts, details, me and newContact

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Contacts from './screens/Contacts';
+import Details from './screens/Details';
+
+const App = () => {
+  return <Contacts />;
+};
+
+export default App;
