70ec4ea3cfacddfcc4dbe3363bd7161bf19034e7
Hugo <hugochen1212@gmail.com>
2017-12-11 14:56:03 -0800

    add List item

--- app/components/ListItem/ListItem.js
+++ app/components/ListItem/ListItem.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
 const ListItem = ({ contact, onPress}) => {
   return (
     <View>
       <Text> {contact.email} </Text>
-      </View>
+    </View>
     )
 };
 
--- app/config/colors.js
+++ app/config/colors.js
@@ -1,3 +1,5 @@
 export default {
-  background: '#ffffff'
+  background: '#ffffff',
+  primaryText: '#525252',
+  subtleText: '#9a9a9a'
 };
