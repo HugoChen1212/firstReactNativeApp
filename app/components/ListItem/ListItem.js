61046fd418a68b7e3e66cff170a89d1a44ff5df1
Hugo <hugochen1212@gmail.com>
2017-12-13 12:43:42 -0800

    add avatar

--- app/components/ListItem/ListItem.js
+++ app/components/ListItem/ListItem.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight, Platform, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import styles, { CHEVRON_SIZE } from './styles';
+
+import colors from '../../config/colors';
+
 const ListItem = ({ contact, onPress}) => {
   return (
-    <View>
-      <Text> {contact.email} </Text>
+    <TouchableHighlight
+    onPress={onPress}
+    >
+    <View style={styles.row}>
+      <Image
+        source={ { uri: contact.picture.thumbnail }}
+        style={ styles.avatar}
+      />
     </View>
+    </TouchableHighlight>
     )
 };
 
