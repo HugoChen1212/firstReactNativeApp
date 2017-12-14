33ec246b62c9d469f6d50305d497d90a3815ea87
Hugo <hugochen1212@gmail.com>
2017-12-14 12:12:47 -0800

    add helper function and apply it in listItem file

--- app/components/ListItem/ListItem.js
+++ app/components/ListItem/ListItem.js
@@ -3,10 +3,12 @@ import { View, Text, TouchableHighlight, Platform, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import styles, { CHEVRON_SIZE } from './styles';
-
+import { capitalizeFirstLetter } from '../../helpers/string';
 import colors from '../../config/colors';
 
 const ListItem = ({ contact, onPress}) => {
+  const firstName = capitalizeFirstLetter(contact.name.first)
+  const lastName =  capitalizeFirstLetter(contact.name.last)
   return (
     <TouchableHighlight
     onPress={onPress}
@@ -17,7 +19,7 @@ const ListItem = ({ contact, onPress}) => {
         style={ styles.avatar}
       />
       <View>
-      <Text> {contact.name.first} {contact.name.last} </Text>
+      <Text> {firstName} {lastName} </Text>
       <Text> {contact.email}</Text>
       </View>
     </View>
