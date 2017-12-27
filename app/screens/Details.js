1be862da6a2339041b5762458b1686489013532b
Hugo <hugochen1212@gmail.com>
2017-12-27 00:29:48 -0800

    create header index and styles js file for user details components

--- app/screens/Details.js
+++ app/screens/Details.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import { Header } from '../components/UserDetails';
+import colors from '../config/colors';
+
 class Details extends Component {
   render() {
     return (
-      <View style={{ flex:1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text> Details Screen</Text>
+      <View style={{ backgroundColor: colors.background }}>
+      <Header />
       </View>
       );
   }
