95fc220e10481957f3113dfc4ed5e4880cb2a5d5
Hugo <hugochen1212@gmail.com>
2017-12-28 19:50:03 -0800

    add Tab navigator

--- app/config/router.js
+++ app/config/router.js
@@ -1,7 +1,10 @@
-import { StackNavigator } from 'react-navigation';
-
+import React from 'react';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 import Contacts from '../screens/Contacts';
 import Details from '../screens/Details';
+import NewContact from '../screens/NewContact';
+import Me from '../screens/Me';
 import { capitalizeFirstLetter } from '../helpers/string';
 
 export const ContactsStack =StackNavigator({
@@ -22,3 +25,29 @@ export const ContactsStack =StackNavigator({
       }),
     },
 });
+
+
+
+export const Tabs = TabNavigator({
+  Contact: {
+    screen: ContactsStack,
+    navigationOptions: {
+      tabBarLabel: 'Contacts',
+      tabBarIcon: ({ tintColor }) => <Icon name="ios-list" size = {35} color={tintColor} />
+    },
+  },
+  NewContact: {
+    screen: NewContact,
+     navigationOptions: {
+      tabBarLabel: 'New Contacts',
+      tabBarIcon: ({ tintColor }) => <Icon name="ios-add" size = {35} color={tintColor} />
+    },
+  },
+  Me: {
+    screen: Me,
+     navigationOptions: {
+      tabBarLabel: 'Me',
+      tabBarIcon: ({ tintColor }) => <Icon name="ios-contact" size = {35} color={tintColor} />
+    },
+  },
+})
